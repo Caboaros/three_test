f745625552d73a5a6f55090b4fa61a95d4de09ce
celsocomcarinho <celsocomcarinho@gmail.com>
2021-07-07 02:28:41 -0300

    added avatar and moon

--- main.js
+++ main.js
@@ -74,8 +74,27 @@ Array(200).fill().forEach(addStar);
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
+//Avatar
+const avatarTexture = new THREE.TextureLoader().load('cookie_eye.jpg');
+const avatar = new THREE.Mesh(
+    new THREE.BoxGeometry(3,3,3),
+    new THREE.MeshBasicMaterial({map: avatarTexture})
+);
 
+scene.add(avatar);
 
+//moon
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: moonTexture,
+        normalMap: normalTexture
+    })
+);
+scene.add(moon);
 
 //animation loop - something like "Update" or "game loop"
 function animate() {
