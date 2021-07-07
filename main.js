7afd69eeeaa387a71b3d141f0e47f3a5a6b1f77c
celsocomcarinho <celsocomcarinho@gmail.com>
2021-07-07 00:16:08 -0300

    standard material with lights

--- main.js
+++ main.js
@@ -22,11 +22,27 @@ renderer.render(scene, camera);
 
 //torus object
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+//basic material
+//const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+//texture material
+const material = new THREE.MeshStandardMaterial({ color: ff6347 });
 const torus = new THREE.Mesh(geometry, material);
 
 //add torus to scene
 scene.add(torus);
+
+//point light
+const pointLight = new THREE.PointLight({ color: 0xFFFFFF });
+pointLight.position.set(5, 5, 5);
+
+//ambient pointLight
+const ambientLight = new THREE.AmbientLight({ color: 0xFFFFFF });
+
+//add lights to the scene
+scene.add(pointLight, ambientLight);
+
+//light helper show where lights are
+const lightHelper = new THREE.PointLightHelper(pointLight);
 
 //animation loop - something like "Update" or "game loop"
 function animate() {
